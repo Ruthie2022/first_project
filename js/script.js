103474c32dd0cebe267a67532dcb8e1d48c062d6
Ruthie2022 <104140885+Ruthie2022@users.noreply.github.com>
2022-06-02 06:31:59 -0700

    my first commit

---
+++ js/script.js
@@ -0,0 +1,481 @@
+// // console.log("SAY HELLO");
+// // take=29
+// // out=93
+// // console.log(take);
+// // console.log(out);
+// // console.log(take+out);
+// // // 2a+b, 3a+b, a+b where a=5 and b=6
+// // a=5;
+// // b=6;
+// // console.log(2*a+b)
+// // console.log(3*a+b)
+// // console.log(a+b);
+// // // this is a variebla
+// // var take=24;
+// // var take=39;
+// // console.log(take);
+// // var date=15;
+// // console.log(date)
+// // // this is a number varieble
+// // let keep=50;
+// //  keep=4;
+// // console.log(keep)
+// // // this is a const varieble
+// // const here=29; 
+// // console.log(here); 
+// // // this ia a string 
+// // let gun="i am a fine girl"
+// //  gun="segun is a ugly boy"
+// // console.log(gun)
+// // // this is Array
+// //  let joseph=["loilio", "bread", "milo", "rice", 3,4,5,6,7,];
+// //  console.log(joseph);
+// //  console.log(joseph.length);
+// //  console.log(joseph[3]);
+// //  console.log(joseph[6]);
+// // //  this enable u add smthing in the back
+// //  joseph.push("eba");
+// //  console.log(joseph[9]);
+// //  console.log(joseph.length);
+// // //  this enables u add smthing in the front
+// //  joseph.unshift("amala");
+// //  console.log(joseph.length);
+// // //  to remove smthing frm the back
+// // joseph.pop()
+// // console.log(joseph);
+// // // to remove smthing from the front
+// // joseph.shift()
+// // console.log(joseph);
+// // // this is the object part
+// //  let ruth={
+// //      name:"Ifeoluwa Emmanuel Samuel", 
+// //      username:"Ruth's boyfrnd",
+// //      password:"ifemi",
+// //      age:33,
+// //      gender:"male"
+// // }
+// //  console.log(ruth);
+// // //  to bring the age out
+// //  console.log(ruth["age"]);
+// //  console.log(ruth["username"]);
+// //  let subject={
+// //      mathematics:50,
+// //      english:95,
+// //      commerce:66,
+// //      governmet:84,
+// //      law:45, 
+// //  }
+// //  console.log(subject);
+// //  console.log(subject["governmet"]);
+// // let object=[
+// //     {name:"segun", age:"31", gender:"male"},
+// //     {name:"ruth", age:"28", gender:"female"},
+// //     {name:"micheal", age:"16", gender:"male"},
+// // ];
+// // console.log(object);
+// // console.log(object[1]["name"]);
+
+// // // conditioner
+// // // let user_input= prompt("what is your age? ");
+// // // console.log(user_input);
+// // // if(user_input >=20){
+// //     // alert("welcome to our site");
+// // // }else{
+// //     // alert("you are not allowed to visit this website")
+// //     // window.location.herf="https://google.com";
+// // // }
+// // // operators
+// // let m=5;
+// //  m=17;
+// // console.log(m)
+// // let r="my name is ruth";
+// // let e="emmanuel"
+// // console.log(r+e)
+// // let p="3" 
+// // let q="5"
+// // console.log(p-q)
+// // let enter="segun"
+// // if(enter == "tunde"){
+// // console.log("enter");
+// // }else{
+// //     console.log("go out")
+// // } 
+// // let entry="tunde"
+// // if(entry =="tunde"){
+// //     console.log("entry"); 
+// // }else if (entry=="kayode"){
+// //     console.log("get out");
+// // }else{
+// //     console.log("go out")
+// // }
+// // let clas=10;
+
+// // if(clas >= 10)
+// // {
+// //     console.log("clas is greater than or equal 10");
+// // }else if(clas < 10){
+// //     console.log("clas is less than 10");
+// // }else{
+// //     console.log("clas is");
+// // }
+
+// // // let user_input =prompt("what is ur age");
+// // // console.log(user_input);
+// // // if (user_input < 10){
+// // //     console.log("it is less than 10");
+// // // }else if(user_input > 10){
+// // //     console.log("it is greater than 10");
+// // // }
+
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+// // // console.log("i am a girl");
+
+// // // looping
+// // for(let i=0; i<10; i++){
+// //     console.log("i am a girl");
+// // }
+// // let total=0;
+// // for (let i=0; i<3; i++){
+// //     total +=4;  
+// // } console.log(total);
+
+// // let current_total =0;
+// // let reg =0;
+// // while(reg<3){
+// //     current_total+=4;
+// //     reg++;
+// // }
+// // console.log(current_total);
+
+// // for(let i=0; i<101; i++){
+// //     console.log(i);
+// // }
+
+// // for( let i=0; i<101; i++)
+// // if(i % 2 == 0){
+// //     console.log("it is an even number" +i);
+// // }else{
+// //     console.log(i);
+// // }
+
+// // // functions
+// // function first_func(){
+// //     console.log("hello world")
+// // }
+
+// // first_func();
+// // function my_sum(x,y){
+// //     console.log(x+y);
+// // }
+
+// // my_sum(3,2);
+// // my_sum(7,5);
+// // my_sum(120,30);
+
+// // function highest_lowest(x){
+// //     for(let i =x; i>0; i--){
+// //         console.log(i);
+// //     }
+// // }
+// // highest_lowest(17);
+
+// // function my_sum(x){
+// //     console.log(x**2); 
+// // }
+
+// // my_sum(11);
+
+// // function my_son(x , y , z){
+// //         console.log(2*x+y-z)
+// //     }
+// // my_son(4,6,2);
+// // my_son(17,22,10);
+
+// // function my_name(name){
+// //     console.log(name)
+// // }
+
+// // my_name("name")
+// // my_name("ruth")
+
+
+// // let selectedcolors =["yellow", "blue"];
+// // selectedcolors[2] = "green";
+
+// // console.log(selectedcolors);
+// // console.log(selectedcolors.length);
+
+// // const companies= [
+// //     {name: "company one", category: "finance", start: 1981, end: 2003},
+// //     {name: "company two", category: "retail", start: 1992, end: 2008},
+// //     {name: "company three", category: "Auto", start: 1999, end: 2007},
+// //     {name: "company four", category: "retail", start: 1989, end: 2010},
+// //     {name: "company five", category: "technolob gy", start: 2009, end: 2014},
+// //     {name: "company six", category: "finance", start: 1987, end: 2010},
+// //     {name: "company seven", category: "auto", start: 1986, end: 1996},
+// // ];
+
+// // const age = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+// // console.log(age.length);
+
+// // for(let i =0; i< companies.length; i++){
+
+// //     console.log(companies[i]);
+// // }
+
+
+// // function isogram(name){
+// // let value=name.split('').filter((item, pos, arr)=> arr.indexOf(item) == pos).length == name.length;
+// // if(value==true){
+// //     console.log("it is an isogram");
+// // }else{
+// //     console.log("it is not an isogram");
+// // }
+// // }
+
+// // isogram("get");
+
+
+// // function is_anagram(pot,top){
+// //     let wods=pot.split("").sort().join("") === top.split("").sort().join("");
+// //     if(wods==true){
+// //         console.log("its an anagram");
+// //     }else{
+// //         console.log("its not an anagram");
+// //     }
+// //   }
+// //   is_anagram("port", "top")
+
+
+// //   let test="management"
+// //   console.log(test.split("").sort().join(""));
+
+// // var a=20;
+
+// // var b=30;
+
+// // var sum=a+b; 
+
+// //   console.log(a + b);
+
+
+// //   var a =10, b =5, sum=a + b;
+
+// //   console.log(sum);
+
+// // var nam = "woman it's God"
+// //  console.log(nam);
+
+
+// //  let string=`i am a girl`   
+
+// //  console.log(string);
+
+
+// //  let send_messages =["emmanuel", "ruth", "segun", "janet"];
+
+// //  function myFunction(nam){
+// //     let message =`
+// //     hello ${nam};
+// //     this is to inform you that there is going to be a mid term break from 18th to 29th of may 2022.
+// //     // `;
+// //     console.log(message);
+// //  }
+
+// //  for (let i=0; i<send_messages.length; i++){
+// //      myFunction(send_messages[i])
+// //  }
+
+
+    
+// // function _category(name, age){ 
+// //     let name_age =`
+// //     my name is ${name}. my age is ${age}.
+// //     `
+// //     console.log(name_age);
+
+// // }
+// // _category("ruth", 20);
+    
+// //  let name_age =[{ name:"ruth", age:40},
+// //  { name:"segun", age:6},
+// //  { name:"micheal", age:12},
+// //  { name:"janet", age:9},
+// //  {name:"beatrice", age:20},
+// //  ];
+// //  console.log(name_age)
+// //  console.log(name_age[1]["name"]);
+
+
+// // for (let i=0; i<name_age.length; i++){
+// //   _category (name_age[i]["name"], name_age[i]["age"])
+// // }
+
+
+
+// // // function data(...num) {
+// // //     let prime = 0;
+// // //     for (let i = 0; i < num.length; i++) {
+// // //         prime += num[i];
+// // //     }
+// // //     console.log(prime);
+// // // };
+
+// // // data(10, 38, 49, 812, 17);
+
+
+// // //  console.log(number)
+
+ 
+
+// // //  send_messages("ruth")
+// // //  send_messages("kim")
+// // //  send_messages("emmanuel")
+// // //  send_messages("segun")
+
+// // function number(...num){
+// //     let sup = 0;
+// //     for(let i = 0; i < num.length; i++){
+// //         sup += num[i];
+// //     }
+// //   console.log(sup);
+// // };
+
+// //  number(30, 39, 40, 292,22);
+
+
+
+
+// //  function hey(){
+// //      console.log("hello word");
+// //  }
+ 
+// //  hey();
+
+
+
+
+
+// // // Bank validation  Function
+// // let code = "123456789992"
+// // let password = "12345"
+// // let user_ammount = "10000"
+
+// // function transfer (pin, ammount, acct){
+// //     if(password != pin){
+// //     console.log("incorect password");
+// //     }
+// //     if(ammount > user_ammount){
+// //         console.log("insufficient fund");
+// //     }
+// //     `let it be ${acct}`
+// // }
+
+// // transfer (12345, 10000, 12345678999);
+
+
+
+// // function agru(numb){
+// // console.log(numb.length);
+// // let corp = 0;
+// // for(let i = 0; i < numb.length; i++){
+// // corp += numb[i];
+// // }
+
+// // console.log(corp)
+ 
+// // }
+
+// // agru([1,2,3,4]);
+
+
+// // // for(let i=0; i<150; i++){
+// //     // console.log(i);
+// // // }
+// let data = [];
+//  function alerts(){
+//      alert("hello world");
+//  }
+
+
+//  function _alertme(){
+//      alert("welcome to me page");
+//  }
+
+//  function showtime(){
+//    let time = new Date();
+//    let wheretoshowthetime= document.getElementById("showtime")
+//    wheretoshowthetime.innerHTML= time;
+
+//  }
+// function showName(){
+//     let name = prompt("add a name");
+//     data.push(name);
+//     let table = document.getElementById("table");
+
+// let row =table.insertRow();
+// let cell1 = row.insertCell();
+// let cell2 = row.insertCell();
+// cell1.innerHTML = data.length;
+// cell2.innerHTML =data[data.length-1]
+// }
+
+
+
+// function _button(){
+//     let table = document.getElementById("_button");
+//     table.style.backgroundColor= "black";
+// }
+// function _boot(){
+//     let table = document.getElementById("_button");
+//     table.style.backgroundColor= "white";
+// }
+$(document).ready(function(){
+
+let something = $(".test");
+
+// something.hide();
+
+let button =$("button");
+let v =$("#_button");
+button.dblclick(function(){
+    v.addClass("changeblack");
+});
+button.click(function(){
+    something.fadeOut(2000);
+});
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
